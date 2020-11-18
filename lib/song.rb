8173426a60fb7464fd7da67b09c47b30fa63bf99
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-17 20:33:16 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+class Song
+
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+
+    def initialize (name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres.push(genre)
+        @@artists.push(artist)
+      end
+
+      def self.count
+        @@count
+      end
+
+      def self.genres
+        @@genres.uniq
+      end
+
+      def self.artists
+        @@artists.uniq
+      end
+
+      def self.genre_count
+            hash = Hash.new(0)
+            @@genres.each{|key| hash[key] += 1}
+            hash
+          end
+
+        def self.artist_count
+         hash = Hash.new(0)
+        @@artists.each{|key| hash[key] += 1}
+         hash
+        end
+
+
+end
